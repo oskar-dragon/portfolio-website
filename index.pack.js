504bbc7e49dab031475a3473ccedce9db63ffb6e
Oskar Dragon <oskartdragon@gmail.com>
2021-10-10 21:56:31 +0100

    added JS, changed colours

--- index.pack.js
+++ index.pack.js
@@ -1,13 +1,16 @@
 const hamburgerBtn = document.querySelector(".nav-toggle");
+const hamburgerEl = document.querySelector(".hamburger");
 const linkEls = document.querySelectorAll(".nav__link");
 const navEl = document.querySelector(".nav");
 
 hamburgerBtn.addEventListener("click", () => {
   navEl.classList.toggle("nav-open");
+  hamburgerEl.classList.toggle("nav-open");
 });
 
 linkEls.forEach(linkEl => {
   linkEl.addEventListener("click", () => {
     navEl.classList.toggle("nav-open");
+    hamburgerEl.classList.toggle("nav-open");
   });
 });
